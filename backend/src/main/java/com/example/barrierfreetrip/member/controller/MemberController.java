7b91653af15c0f00c2df058d2cf8955c5ebeff23
mjjunng <mjjunghp@naver.com>
2023-05-18 14:58:39 +0900

    feat: naver login

--- backend/src/main/java/com/example/barrierfreetrip/member/controller/MemberController.java
+++ backend/src/main/java/com/example/barrierfreetrip/member/controller/MemberController.java
@@ -1,12 +1,12 @@
 package com.example.barrierfreetrip.member.controller;
 
 import com.example.barrierfreetrip.member.dto.MemberResponseDto;
-import com.example.barrierfreetrip.member.service.KakaoMemberService;
+import com.example.barrierfreetrip.member.service.OauthMemberService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
-    private final KakaoMemberService kakaoMemberService;
+    private final OauthMemberService oauthMemberService;
 
     @GetMapping("/welcome")
     public String healthCheck() {
@@ -24,11 +25,19 @@ public class MemberController {
     }
 
 
-    @GetMapping("/ouath/kakao")
-    public ResponseEntity kakaoLoin(@RequestParam("code") String code, HttpServletResponse response)
+    @GetMapping("/oauth/kakao")
+    public ResponseEntity kakaoLoin(@RequestParam("code") String code)
                                         throws JsonProcessingException {
+        System.out.println("kakao: " + code);
+        MemberResponseDto memberResponseDto = oauthMemberService.oauthLogin(code, "kakao");
+        return ResponseEntity.status(HttpStatus.OK).body(memberResponseDto);
+    }
 
-        MemberResponseDto memberResponseDto = kakaoMemberService.kakaoLogin(code, response);
+    @GetMapping("/oauth/naver")
+    public ResponseEntity naverLoin(@RequestParam("code") String code)
+            throws JsonProcessingException {
+        System.out.println("naver: " + code);
+        MemberResponseDto memberResponseDto = oauthMemberService.oauthLogin(code, "naver");
         return ResponseEntity.status(HttpStatus.OK).body(memberResponseDto);
     }
 
--- backend/src/main/java/com/example/barrierfreetrip/member/service/OauthMemberService.java
+++ backend/src/main/java/com/example/barrierfreetrip/member/service/OauthMemberService.java
@@ -4,14 +4,13 @@ import com.example.barrierfreetrip.member.domain.Member;
 import com.example.barrierfreetrip.member.dto.MemberResponseDto;
 import com.example.barrierfreetrip.member.dto.SocialMemberDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.security.core.Authentication;
 
 import javax.servlet.http.HttpServletResponse;
 
-public interface KakaoMemberService {
-    public MemberResponseDto kakaoLogin(String code, HttpServletResponse response) throws JsonProcessingException;
+public interface OauthMemberService {
+    public MemberResponseDto oauthLogin(String code, String type) throws JsonProcessingException;
     public String getAccessToken(String code) throws JsonProcessingException;
-    public SocialMemberDto getKakaoMemberInfo(String accessToken) throws JsonProcessingException;
-    public Member registerKakaoMemberIfNeed(SocialMemberDto kakaoMemberDto);
+    public SocialMemberDto getOauthMemberInfo(String accessToken) throws JsonProcessingException;
+    public Member registerOauthMemberIfNeed(SocialMemberDto kakaoMemberDto);
 
 }
