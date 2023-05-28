8d24cc25720acc0c5955c7c94e8e1c9d2ad085e9
Hwanju Lee <hwanju1596@gmail.com>
2023-05-29 07:03:13 +0900

    feat: StayCard Align Center 1. Align Center 2. 데이터 4개로 수정

--- fronend/pages/home/index.tsx
+++ fronend/pages/home/index.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
 import {StayCard} from '../components/stayCard';
 import {CareCard} from '../components/careCard';
-import {FlatList, ScrollView, Text, View} from 'react-native';
+import {
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 const DATA = [
-  {
-    imgSrc: 'http://tong.visitkorea.or.kr/cms/resource/60/2678560_image2_1.jpg',
-    title: 'Ttitle1',
-    description: 'Lorem ipsum dolor sit amet',
-    grade: '4.3',
-    gradeCount: 423,
-    location: '서울역 도보 7분',
-  },
-  {
-    imgSrc: 'http://tong.visitkorea.or.kr/cms/resource/60/2678560_image2_1.jpg',
-    title: 'Ttitle2',
-    description: 'Lorem ipsum dolor sit amet',
-    grade: '4.3',
-    gradeCount: 423,
-    location: '서울역 도보 7분',
-  },
-  {
-    imgSrc: 'http://tong.visitkorea.or.kr/cms/resource/60/2678560_image2_1.jpg',
-    title: '신라호텔',
-    description: 'Lorem ipsum dolor sit amet',
-    grade: '4.3',
-    gradeCount: 423,
-    location: '서울역 도보 7분',
-  },
   {
     imgSrc: 'http://tong.visitkorea.or.kr/cms/resource/60/2678560_image2_1.jpg',
     title: '신라호텔',
@@ -65,8 +48,14 @@ const DATA = [
 export const Home = () => {
   return (
     <ScrollView>
+      <View style={homeStyles.iconsContainer}></View>
       <FlatList
+        contentContainerStyle={{
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
         data={DATA}
+        scrollEnabled={false}
         renderItem={({item}) => (
           <StayCard
             imgSrc={item.imgSrc}
@@ -84,3 +73,17 @@ export const Home = () => {
     </ScrollView>
   );
 };
+
+const homeStyles = StyleSheet.create({
+  stayCardContainer: {
+    width: 325,
+    height: 454,
+  },
+  iconsContainer: {
+    width: 250,
+    height: 136,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+});
