46e0b6ab403169b8b3a7550d03bfecbb35d31921
mjjunng <52596617+mjjunng@users.noreply.github.com>
2024-01-07 21:11:54 +0900

    refactor: main에서 코드 실행

--- data/wheelchairCharger.py
+++ data/wheelchairCharger.py
@@ -1,14 +1,8 @@
 import json
 import pymysql
 
-def save_mysql_carger(db, data):
-    conn = pymysql.connect(host='localhost',
-                           user='root',
-                           password='0422',
-                           db=db,
-                           charset='utf8')
-    cursor = conn.cursor()
-    sql = "insert into wheelchair_charger(id, title, sido, sigungu, areaCode, addr, weekdayOpen, " \
+def saveMysqlCarger(data, cursor, conn):
+    sql = "insert into wheelchairCharger(id, title, sido, sigungu, areaCode, addr, weekdayOpen, " \
           "weekdayClose, weekendOpen, weekendClose, holidayOpen, holidayClose, possible, air, phoneCharge, tel, " \
           "mapx, mapy) " \
           "values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
@@ -17,37 +11,52 @@ def save_mysql_carger(db, data):
     conn.commit()
     conn.close()
 
-file_path = "./wheelchairChargerData.json"
+def saveWheelchairCharger(host, user, password, db, charset):
+    conn = pymysql.connect(host=host,
+                           user=user,
+                           password=password,
+                           db=db,
+                           charset=charset)
+    cursor = conn.cursor()
 
-idx = 0
-with open(file_path, 'r', encoding='utf-8') as file:
-    json_data = json.load(file)
-    data = json_data["records"]
+    file_path = "wheelchairChargerData.json"
 
-    for d in data:
-        title = d["시설명"]
-        sido = d["시도명"]
-        sigungu = d["시군구명"]
-        areaCode = d["시군구코드"]
-        addr = d["소재지도로명주소"]
-        addr2 = d["소재지지번주소"]
-        weekdayOpen = d["평일운영시작시각"]
-        weekdayClose = d["평일운영종료시각"]
-        weekendOpen = d["토요일운영시작시각"]
-        weekendClose = d["토요일운영종료시각"]
-        holidayOpen = d["공휴일운영시작시각"]
-        holidayClose = d["공휴일운영종료시각"]
-        possible = d["동시사용가능대수"]
-        air = d["공기주입가능여부"]
-        phoneCharge = d["휴대전화충전가능여부"]
-        tel = d["관리기관전화번호"]
-        mapx = d["경도"]
-        mapy = d["위도"]
+    idx = 0
+    wheelchairCnt= 0
 
-        if not addr:
-            addr = addr2
+    with open(file_path, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+        data = json_data["records"]
 
-        save_mysql_carger("barrier_free_trip", tuple([idx, title, sido, sigungu, areaCode, addr, weekdayOpen,
-                                                      weekdayClose, weekendOpen, weekendClose, holidayOpen, holidayClose,
-                                                      possible, air, phoneCharge, tel, mapx, mapy]))
-        idx += 1
+        for d in data:
+            title = d["시설명"]
+            sido = d["시도명"]
+            sigungu = d["시군구명"]
+            areaCode = d["시군구코드"]
+            addr = d["소재지도로명주소"]
+            addr2 = d["소재지지번주소"]
+            weekdayOpen = d["평일운영시작시각"]
+            weekdayClose = d["평일운영종료시각"]
+            weekendOpen = d["토요일운영시작시각"]
+            weekendClose = d["토요일운영종료시각"]
+            holidayOpen = d["공휴일운영시작시각"]
+            holidayClose = d["공휴일운영종료시각"]
+            possible = d["동시사용가능대수"]
+            air = d["공기주입가능여부"]
+            phoneCharge = d["휴대전화충전가능여부"]
+            tel = d["관리기관전화번호"]
+            mapx = d["경도"]
+            mapy = d["위도"]
+
+            if not addr:
+                addr = addr2
+
+            saveMysqlCarger(tuple([idx, title, sido, sigungu, areaCode, addr, weekdayOpen,
+                                                          weekdayClose, weekendOpen, weekendClose, holidayOpen, holidayClose,
+                                                          possible, air, phoneCharge, tel, mapx, mapy]), cursor, conn)
+            idx += 1
+            wheelchairCnt += 1
+
+    print("=====FINISH SAVE WHEELCHAIR DATA=====")
+    print("=====        SAVE TOTAL           =====")
+    print("wheelchair: {}".format(wheelchairCnt))
