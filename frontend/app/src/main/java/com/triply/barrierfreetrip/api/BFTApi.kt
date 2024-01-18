bc47aed818bf7b7cb0950e90e18367aea6feb2bc
JinWoo <seanjjang98@gmail.com>
2024-01-18 20:59:37 +0900

    feat: Prepare for api
    + 관광시설 리스트 API 추가
    + 관광시설 상세보기 API 추가
    + 충전기 리스트 API 추가
    + 충전기 상세보기 API 추가
    + 돌봄여행 리스트 API 추가
    + 렌탈 서비스 리스트 API 추가

---
+++ frontend/app/src/main/java/com/triply/barrierfreetrip/api/BFTApi.kt
@@ -0,0 +1,54 @@
+package com.triply.barrierfreetrip.api
+
+import com.triply.barrierfreetrip.data.CareTour
+import com.triply.barrierfreetrip.data.Charger
+import com.triply.barrierfreetrip.data.ChargerDetail
+import com.triply.barrierfreetrip.data.RentalServicePlace
+import com.triply.barrierfreetrip.data.TourFacility
+import com.triply.barrierfreetrip.data.TourFacilityDetail
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface BFTApi {
+    // API for Review
+
+    // API for tourist facilities
+    @GET("/tourist-facilities/{contentTypeId}/{areaCode}/{sigunguCode}")
+    suspend fun getTourFcltList(
+        @Path(value = "contentTypeId") typeId : String,
+        @Path(value = "areaCode") areaCode : String,
+        @Path(value = "sigunguCode") bigPlaceCode : String
+    ) : Call<List<TourFacility>>
+
+
+    @GET("/tourist-facilities/{contentId}")
+    suspend fun getTourFcltDetail(
+        @Path(value = "contentId") contentId : String
+    ) : Call<TourFacilityDetail>
+
+    // API for Charger
+    @GET("/chargers/{areaCode}")
+    suspend fun getChargerList(
+        @Path(value = "areaCode") areaCode: String
+    ) : Call<List<Charger>>
+
+    @GET("/chargers/info/{contentId}")
+    suspend fun getChargerDetail(
+        @Path(value = "contentId") contentId: String
+    ) : Call<ChargerDetail>
+
+    @GET("/care-services/{sido}/{sigungu}")
+    suspend fun getCareTourList(
+        @Path(value = "sido") bidPlaceCode : String,
+        @Path(value = "sigungu") smallPlaceCode : String
+    ) : Call<List<CareTour>>
+
+    @GET("/rentals/{sido}/{sigungu}")
+    suspend fun getRentalServiceList(
+        @Path(value = "sido") bidPlaceCode : String,
+        @Path(value = "sigungu") smallPlaceCode : String
+    ) : Call<List<RentalServicePlace>>
+
+}
