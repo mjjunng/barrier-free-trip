3b7b4a45f56c967b73ae2c05c2b25102eb60a622
JinWoo <seanjjang98@gmail.com>
2024-02-15 22:37:47 +0900

    feat: add Adapter
    two adapter for recyclerview

---
+++ frontend/app/src/main/java/com/triply/barrierfreetrip/adapter/InfoListAdapter.kt
@@ -0,0 +1,61 @@
+package com.triply.barrierfreetrip.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.accessibility.AccessibilityManager.AccessibilityServicesStateChangeListener
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.triply.barrierfreetrip.R
+import com.triply.barrierfreetrip.data.InfoListDto
+import com.triply.barrierfreetrip.databinding.ItemInfoListBinding
+
+// 화면에 표시만 하면 되는 경우 사용하는 기본 Adapter
+open class InfoListAdapter : RecyclerView.Adapter<ListViewHolder>() {
+    private lateinit var binding : ItemInfoListBinding
+    var infoList = ArrayList<InfoListDto>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_info_list, parent, false
+        )
+        return ListViewHolder(binding)
+    }
+
+    override fun getItemCount() = infoList.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(infoList[position])
+    }
+}
+
+// 충전기처럼 다른 페이지로 넘어가야 하는 경우 사용하는 Adapter
+class InfoListClickAdapter : InfoListAdapter() {
+    private lateinit var itemClickListener: InfoListClickAdapter.OnItemClickListener
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        super.onBindViewHolder(holder, position)
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, position)
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onClick(view: View, position: Int)
+    }
+
+    fun setItemClickListener(itemClickListener: OnItemClickListener) {
+        this.itemClickListener = itemClickListener
+    }
+}
+
+class ListViewHolder(
+    private val binding : ItemInfoListBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item : InfoListDto) {
+
+        binding.tbListLike.isChecked = item.like
+    }
+}
---
+++ frontend/app/src/main/java/com/triply/barrierfreetrip/adapter/InfoSquareAdapter.kt
@@ -0,0 +1,50 @@
+package com.triply.barrierfreetrip.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.triply.barrierfreetrip.R
+import com.triply.barrierfreetrip.data.InfoSquareDto
+import com.triply.barrierfreetrip.databinding.ItemInfoSquareBinding
+
+class InfoSquareAdapter : RecyclerView.Adapter<SquareViewHolder>() {
+    private lateinit var binding : ItemInfoSquareBinding
+    private lateinit var itemClickListener: InfoSquareAdapter.OnItemClickListener
+    var infoList = ArrayList<InfoSquareDto>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int
+    ): SquareViewHolder {
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_info_square, parent, false
+        )
+        return SquareViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SquareViewHolder, position: Int) {
+        holder.bind(infoList[position])
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, position)
+        }
+    }
+    override fun getItemCount() = infoList.size
+
+    interface OnItemClickListener {
+        fun onClick(view: View, position: Int)
+    }
+
+    fun setItemClickListener(itemClickListener: OnItemClickListener) {
+        this.itemClickListener = itemClickListener
+    }
+}
+
+class SquareViewHolder(
+    private val binding : ItemInfoSquareBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item : InfoSquareDto) {
+        binding.squareItem = item
+    }
+}
