50517294e5696e486b66bd0a38e1226da24082f3
mjjunng <52596617+mjjunng@users.noreply.github.com>
2024-03-31 00:00:22 +0900

    Revert "240328 Pull Request - App"

--- frontend/app/src/main/java/com/triply/barrierfreetrip/adapter/InfoListAdapter.kt
+++ frontend/app/src/main/java/com/triply/barrierfreetrip/adapter/InfoListAdapter.kt
@@ -1,19 +1,16 @@
 package com.triply.barrierfreetrip.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.triply.barrierfreetrip.R
 import com.triply.barrierfreetrip.data.InfoListDto
 import com.triply.barrierfreetrip.databinding.ItemInfoListBinding
 
 // 화면에 표시만 하면 되는 경우 사용하는 기본 Adapter
 open class InfoListAdapter(var infoList : ArrayList<InfoListDto>) : RecyclerView.Adapter<ListViewHolder>() {
-
     private lateinit var binding : ItemInfoListBinding
     //var infoList = ArrayList<InfoListDto>()
 
@@ -34,7 +31,6 @@ open class InfoListAdapter(var infoList : ArrayList<InfoListDto>) : RecyclerView
 
 // 충전기처럼 다른 페이지로 넘어가야 하는 경우 사용하는 Adapter
 class InfoListClickAdapter(infoList: ArrayList<InfoListDto>) : InfoListAdapter(infoList) {
-
     private lateinit var itemClickListener: InfoListClickAdapter.OnItemClickListener
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
--- frontend/app/src/main/java/com/triply/barrierfreetrip/api/RetroInstance.kt
+++ frontend/app/src/main/java/com/triply/barrierfreetrip/api/RetroInstance.kt
@@ -1,29 +1,30 @@
 package com.triply.barrierfreetrip.api
 
-import okhttp3.OkHttpClient
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import com.triply.barrierfreetrip.BFTApplication
+import com.triply.barrierfreetrip.MainActivity
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetroInstance {
-    val BASE_URL = "http://10.0.2.2:8080"
+    private val BASE_URL = "http://10.0.2.2:8080"
 
-    private val httpClient = OkHttpClient.Builder()
-    private val builder = Retrofit
+    private var appInfo = BFTApplication.ApplicationContext().applicationContext.packageManager.getApplicationInfo(
+        BFTApplication.ApplicationContext().applicationContext.packageName, PackageManager.GET_META_DATA
+    )
+    private val KAKAO_KEY = appInfo.metaData.getString("KAKAO_KEY").toString()
+    private val NAVER_KEY = appInfo.metaData.getString("NAVER_KEY").toString()
+    private val KAKAO_URL = "http://localhost:8080/oauth/kakao&response_type=code"
+    private val NAVER_URL = "http://localhost:8080/oauth/naver"
+
+    private val client: Retrofit = Retrofit
         .Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-    private var client = builder.build()
+        .build()
 
-    fun getInstance() : Retrofit = client
-    fun createBFTApi(token : String) : BFTApi {
-        val interceptor = AuthInterceptor(token)
-
-        if (!httpClient.interceptors().contains(interceptor)) {
-            httpClient.addInterceptor(interceptor)
-            builder.client(httpClient.build())
-            client = builder.build()
-        }
-        return client.create(BFTApi::class.java)
+    fun getInstance() : Retrofit {
+        return client
     }
 }
-
